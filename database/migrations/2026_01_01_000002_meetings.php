ac368a899a8c3a80df57eec0b01ba7c5f03f457c
ahmednour1234 <ahmednour5999@gmail.com>
2026-02-08 12:54:48 +0200

    N+1

--- database/migrations/2026_01_01_000002_meetings.php
+++ database/migrations/2026_01_01_000002_meetings.php
@@ -8,6 +8,10 @@ return new class extends Migration
 {
     public function up(): void
     {
+        if (Schema::hasTable('meetings')) {
+            return;
+        }
+
         Schema::create('meetings', function (Blueprint $table) {
             $table->id();
 
