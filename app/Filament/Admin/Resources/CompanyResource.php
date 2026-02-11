0cd2c03bbe048ba5285c9c53d510ee3f8bae8465
ahmednour1234 <ahmednour5999@gmail.com>
2026-02-11 11:13:24 +0200

    N+1

--- app/Filament/Admin/Resources/CompanyResource.php
+++ app/Filament/Admin/Resources/CompanyResource.php
@@ -20,17 +20,6 @@ class CompanyResource extends Resource
 
     protected static ?string $navigationLabel = 'Companies';
 
-    public static function shouldRegisterNavigation(): bool
-    {
-        $user = Auth::user();
-        
-        if (! $user || ! $user->is_active) {
-            return false;
-        }
-
-        return $user->role === 'admin';
-    }
-
     public static function getEloquentQuery(): \Illuminate\Database\Eloquent\Builder
     {
         return parent::getEloquentQuery()->with(['owner', 'event', 'package', 'country', 'createdBy', 'bookedBy']);
